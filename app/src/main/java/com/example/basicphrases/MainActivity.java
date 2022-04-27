c6010b146a1b95d39aa1ded2fae3522d6d042bee
github_LibnaZulkarnain <libnazulkarnain20@gmail.com>
2022-04-27 22:37:50 +0700

    first commit

---
+++ app/src/main/java/com/example/basicphrases/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.basicphrases;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void buttonPressed(View view){
+        int id = view.getId();
+        String ourId = "";
+
+        ourId = view.getResources().getResourceEntryName(id);
+        int resourceId = getResources().getIdentifier(ourId, "raw", "com.example.playaudioo");
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, resourceId);
+        mediaPlayer.start();
+    }
+
+
+}
